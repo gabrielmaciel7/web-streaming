d9d1f68afbee4425fe8c906cb8d8418b16cbe420
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-09-21 10:57:52 -0300

    add slider

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styles';
-import VideoCard from './components/VideoCard';
 
-function VideoCardGroup({
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
+import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slider';
+
+function Carousel({
   ignoreFirstVideo,
   category,
 }) {
+
   const categoryTitle = category.title;
   const categoryColor = category.color;
   const categoryExtraLink = category.link_extra;
   const videos = category.videos;
+
   return (
     <VideoCardGroupContainer>
       {categoryTitle && (
@@ -24,25 +28,25 @@ function VideoCardGroup({
           }
         </>
       )}
-      <VideoCardList>
+      <Slider>
         {videos.map((video, index) => {
           if (ignoreFirstVideo && index === 0) {
             return null;
           }
 
           return (
-            <li key={video.title}>
+            <SliderItem key={video.title}>
               <VideoCard
                 videoTitle={video.title}
                 videoURL={video.url}
                 categoryColor={categoryColor}
               />
-            </li>
+            </SliderItem>
           );
         })}
-      </VideoCardList>
+      </Slider>
     </VideoCardGroupContainer>
   );
 }
 
-export default VideoCardGroup;
+export default Carousel;
--- src/pages/Register/Category/index.js
+++ src/pages/Register/Category/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import PageDefault from "../../../components/PageDefault";
 
